98ae767c06e4590f124ee8636de836578caa0fd8
kouzukek <kouzukekouryu@gmail.com>
2015-02-27 03:38:34 +0900

    update-check.py

--- update-check.py
+++ update-check.py
@@ -6,7 +6,7 @@ from socket import gethostname
 import re
 import sys
 
-mailto   = "user@hoge.com"
+mailto   = sys.argv[1] if len(sys.argv) == 2 else None
 mailfrom = "root@%s" % gethostname()
 
 def mail(content):
@@ -19,7 +19,7 @@ def mail(content):
     check_output(["/usr/sbin/sendmail", "-t"],
                  input = msg.as_string().encode("utf-8"))
     
-def apt_upgradable_list():
+def apt_list_upgradable():
     call(["apt","update"],stdout = DEVNULL,stderr=DEVNULL)
     out = check_output(
             ["apt","list","--upgradable"],
@@ -28,11 +28,12 @@ def apt_upgradable_list():
     return out[out.find("\n")+1:]
 
 if __name__=="__main__":
-    ret=apt_upgradable_list()
+    ret=apt_list_upgradable()
     if len(ret)>0:
         print("[APT] upgradables for %s"%gethostname())
         print("-"*20)
         print(ret)
-        mail(ret)
+        if mailto:
+            mail(ret)
     else:
         print("Nothing will be mailed.")
